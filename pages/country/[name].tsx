73adfed08f7cd0b642f3833fd456f6609201bd9e
ajihub <ajisoriano02@gmail.com>
2021-10-13 23:05:34 +0800

    EB-201 Feature- Design, Responsive, EB-200 Feature- Click on a country to see more details

--- pages/country/[name].tsx
+++ pages/country/[name].tsx
@@ -1,4 +1,10 @@
+import Head from "next/dist/shared/lib/head";
+import Link from "next/dist/client/link";
 import { NextPage } from "next";
+import { addCommas } from "..";
+import { IoArrowBackOutline } from "react-icons/io5";
+import { RootDiv, SubRoot, TopDiv, DetailsDiv, FlexDiv, TextDiv, BorderDiv, BackButton, BorderButton, CountryImage} from "../../styles/styles-detailspage";
+
 
 export const getStaticPaths = async () =>{
     const res = await fetch('https://restcountries.com/v3.1/all');
@@ -43,9 +49,87 @@ export const getStaticProps = async (context:any) =>{
 }
 
 const CountryDetails: NextPage <{country:any, neighbors:any}> = ({country, neighbors}) => {
+
+    let NativeNameArr:Array<string>;
+    if('nativeName' in country[0].name){
+     NativeNameArr = (Object.values(country[0].name.nativeName)).map((val:any)=> val.common)
+    }else{
+     NativeNameArr = ['No native name']
+    }
+
+    let CurrencyArr:Array<string>;
+    if('currencies' in country[0]){
+        CurrencyArr = (Object.values(country[0].currencies)).map((val:any)=> val.name)
+    }else{
+        CurrencyArr = ['No currency']
+    }
+
+    let LanguagesArr:Array<string>;
+    if('languages' in country[0]){
+        LanguagesArr= (Object.values(country[0].languages))
+    }else{
+        LanguagesArr = ['No languages']
+    }
+    
     return(
-        <>
-        </>
+<RootDiv>
+    <Head>
+        <title>REST API Countries | Details</title>
+        <meta name="description" content="rest api countries challenge" />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <SubRoot>
+        <TopDiv>
+            <Link href={'/'}>
+                <BackButton>
+                    <IoArrowBackOutline size={20}/>Back
+                </BackButton>
+            </Link>
+        </TopDiv>
+
+        <DetailsDiv>
+            <CountryImage src={country[0].flags.png} alt={country[0].name.common}/>
+            <div> 
+                <div>
+                    <TextDiv>
+                    <h2>{country[0].name.common}</h2>
+                    </TextDiv>
+                </div>
+                <FlexDiv>
+                    <TextDiv>
+                        <p><b>Native Name:</b> {NativeNameArr.join(", ")}</p>
+                        <p><b>Population:</b> {addCommas(country[0].population)}</p>
+                        <p><b>Region:</b> {country[0].region}</p>
+                        <p><b>Sub Region:</b> {country[0].subregion}</p>
+                        <p><b>Capital:</b> {country[0].capital}</p>
+                    </TextDiv>
+
+                    <TextDiv>
+                        <p><b>Top Level Domain:</b> {country[0].tld}</p>
+                        <p><b>Currencies:</b> {CurrencyArr.join(", ")}</p>
+                        <p><b>Languages:</b> {LanguagesArr.join(", ")}</p>
+                    </TextDiv>
+                </FlexDiv>
+                <div>   
+                    <TextDiv>
+                        <BorderDiv>
+                            <p><b>Border Countries: </b></p>
+                            <div>
+                                {neighbors.map((neighbors:any) => (
+                                <Link href={'/country/'+neighbors.name} key={neighbors.name}>
+                                    <BorderButton>{neighbors.name}</BorderButton>
+                                </Link>
+                                ))}
+                            </div>
+                        </BorderDiv>
+                </TextDiv>
+                </div>  
+            </div>
+            
+        </DetailsDiv>
+    </SubRoot>    
+</RootDiv>    
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/dist/client/link';
+import { RootDiv, IndexDiv, IndexFlexDiv, CardTestDiv, SubDiv, SubPs, HeadPs } from '../styles/styles-index';
 
 export const getStaticProps = async () =>{
 
@@ -12,11 +14,39 @@ export const getStaticProps = async () =>{
   }
 }
 
+export const addCommas =(val:number)=>{
+  return val.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}            
+
 
 const Home: NextPage <{allcountries: Array<any>}> = ({allcountries}) => {
   return (
-    <>
-    </>
+    <RootDiv>
+      <Head>
+          <title>REST API Countries | by Aji</title>
+          <meta name="index" content="rest api countries challenge" />
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <IndexDiv>
+        <IndexFlexDiv>
+            {allcountries.map((allcountries: any, key) =>(
+              <Link href={'/country/'+allcountries.name.common} key={allcountries.name.official}>
+                <CardTestDiv key ={key}>
+                  <Image src={allcountries.flags.png} alt={allcountries.name.common} width={265} height={161}/>
+                  <SubDiv>
+                    <HeadPs>{allcountries.name.common}</HeadPs>
+                    <SubPs><b>Population: </b>{addCommas(allcountries.population)}</SubPs> 
+                    <SubPs><b>Region: </b>{allcountries.region}</SubPs>
+                    <SubPs><b>Capital: </b>{allcountries.capital}</SubPs>
+                  </SubDiv>
+                </CardTestDiv>
+              </Link> 
+            ))}
+        </IndexFlexDiv>
+      </IndexDiv>
+
+    </RootDiv>
   )
 }
 
